0a2434cfd88bbdde87c2d594b6c9c904cb4f5ac2
Barrantes <barrantesduranerickamariel@gmail.com>
2026-06-30 00:16:28 -0600

    adaptación

--- src/main/java/com/link360/controller/HomeController.java
+++ src/main/java/com/link360/controller/HomeController.java
@@ -10,6 +10,6 @@ public class HomeController {
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("activeMenu", "dashboard");
-        return "home";
+        return "home/home";
     }
 }
